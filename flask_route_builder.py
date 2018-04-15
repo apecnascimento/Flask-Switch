67e5ac8b5038b87e6c772e1eded1a2cd973d4afd
pedro <apedroed88@gmail.com>
2018-04-14 21:51:48 -0300

    Change of names for better understanding.

--- flask_route_builder.py
+++ flask_route_builder.py
@@ -3,26 +3,32 @@ import pkgutil
 from flask import current_app
 
 
-def format_route(blueprint):
-    return ('blueprints.{0}.main'.format(blueprint), blueprint)
-
-
 class RouterBuilder(object):
-    def __init__(self, app=None):
+    def __init__(self, app=None, blueprints_package=None):
         self._app = app
+
+        #: The name where blueprints are.
+        self._blueprints_package = blueprints_package or 'blueprints'
+
         if app is not None:
             self.init_app(app)
 
+    def _format_route(self, blueprint, blueprints_package):
+        return '{0}.{1}.main'.format(blueprints_package, blueprint), blueprint
+
     def init_app(self, app):
         app.config.setdefault('ROUTER_BUILDER_URL_PREFIX', None)
-        # List all blueprints in the application blueprints package and return a list of tuples with (url, module_name)
-        blue_prints = [format_route(module_name) for _, module_name, _ in pkgutil.iter_modules(['blueprints'])]
-        for url, module_name in blue_prints:
-            blue_print = __import__(module, globals(), locals(), [module_name], 0)
+
+        #: List all blueprints in the application blueprints package and return a list of tuples with (url, module_name)
+        blueprints = [self._format_route(module_name, self._blueprints_package) for _, module_name, _ in
+                      pkgutil.iter_modules(['blueprints'])]
+
+        for module_namespace, module_name in blueprints:
+            blueprint = __import__(module_namespace, globals(), locals(), [module_name], 0)
             if current_app.config['ROUTER_BUILDER_URL_PREFIX'] is not None:
-                self._flask_app.register_blueprint(getattr(blue_print, module_name),
+                self._flask_app.register_blueprint(getattr(blueprint, module_name),
                                                    url_prefix='/{0}/{1}'.format(
                                                        current_app.config['ROUTER_BUILDER_URL_PREFIX'], module_name))
             else:
-                self._flask_app.register_blueprint(getattr(blue_print, module_name),
+                self._flask_app.register_blueprint(getattr(blueprint, module_name),
                                                    url_prefix='/{0}'.format(module_name))
